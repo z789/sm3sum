69107d90d788c7fa060a5eab1b63df20f05ecd73
z789 <cfcjl@163.com>
2023-08-31 10:15:50 +0800

    fix short opt 't' and 'h'

--- sm3sum.c
+++ sm3sum.c
@@ -255,7 +255,7 @@ int main(int argc, char **argv)
 	while (1) {
 		int option_index = 0;
 
-		c = getopt_long(argc, argv, "c", long_options, &option_index);
+		c = getopt_long(argc, argv, "cht", long_options, &option_index);
 		if (c == -1)
 			break;
 		switch (c) {
